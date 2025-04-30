543eb30975acbe6b52c06151a020be87a838d874
hoanghaiduong <hoanghaiduong1711@gmail.com>
2025-04-30 16:54:42 +0700

    ok

--- Program.cs
+++ Program.cs
@@ -13,10 +13,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.WebHost.ConfigureKestrel(serverOptions =>
-        {
-            serverOptions.ListenAnyIP(5876); // Lắng nghe tất cả IP
-        });
+      
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
