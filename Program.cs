61736c03c692cea6a1fe6b586809581da3e5d11d
hoanghaiduong <hoanghaiduong1711@gmail.com>
2025-04-30 17:38:16 +0700

    finish

--- Program.cs
+++ Program.cs
@@ -21,31 +21,12 @@ public class Program
         app.UseSwagger();
         app.UseSwaggerUI();
 
-
         // app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-        app.MapGet("/", () => "Hello World!");
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
+      
+        
 
         app.Run();
     }
